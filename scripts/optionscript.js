6dbd95f86ed8b721e88509d292a77312f93ea94d
Karnbir S <karnbirsaini@hotmail.com>
2019-03-19 13:09:15 -0400

    Adding Local Storage
    Added 4 Keys: Grade, Gender, Clubs, Student Number

--- scripts/optionscript.js
+++ scripts/optionscript.js
@@ -9,4 +9,8 @@ annGender = document.getElementById("gender").value;
 annClubs = document.getElementById("clubs").value;
 studentNumber = document.getElementById("student#").value;
 alert(annGrade + annGender + annClubs + studentNumber);
+localStorage.setItem("Grade:", annGrade);
+localStorage.setItem("Gender:", annGender);
+localStorage.setItem("Clubs:", annClubs);
+localStorage.setItem("Student Number:", studentNumber);
 }
