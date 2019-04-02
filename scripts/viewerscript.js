2d227ec3d5cd3cdd4359868c9bba771e5f65f091
Karnbir S <karnbirsaini@hotmail.com>
2019-04-02 14:24:46 -0400

    Display values from LocalStorage in Student Viewer
    viewerscript.js:
    - creating 9 initial variables (student and announcement)
    - function readStorage which getItem from localStorage keys into variables and puts annTitle and annDetails vars into title and details id on html
    
    viewerstyle.css:
    - rearrange img to top
    - h3 tag is coloured to distinguish Title from Details
    - announcement 1 has a solid border
    
    viewer.html:
    - onload - readStorage()
    - div has id of announcement1
    - h3 and p element for announcement display

---
+++ scripts/viewerscript.js
@@ -0,0 +1,24 @@
+var studentGrade;
+var studentGender;
+var studentClub;
+var studentNumber;
+var annTitle;
+var annDetails;
+var annGrade;
+var annGender;
+var annClub;
+
+function readStorage() {
+	studentGrade = localStorage.getItem("Student Grade:");
+	studentGender = localStorage.getItem("Student Gender:");
+	studentClub = localStorage.getItem("Student Club:");
+	studentNumber = localStorage.getItem("Student Number:");
+	annTitle = localStorage.getItem("Title:");
+	annDetails = localStorage.getItem("Details:");
+	annGrade = localStorage.getItem("Grade:");
+	annGender = localStorage.getItem("Gender:");
+	annClub = localStorage.getItem("Club:");
+	document.getElementById("title").innerHTML = annTitle;
+	document.getElementById("details").innerHTML = annDetails;
+}
+	
