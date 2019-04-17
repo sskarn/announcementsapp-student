145e5c50868668847b2dfb9ada7d8add9e472145
Karnbir S <karnbirsaini@hotmail.com>
2019-04-16 22:53:58 -0400

    cleaning up and removing unnecessary components
    optionscript.js:
    - removed commented out alert to test for variables and replaced with an alert to inform student that their profile has been saved
    - fixed tabbing inside function
    
    optionstyle.css:
    - added border to div for profileselector
    
    options.html:
    - removed unnecessary div elements and consolidated into 3 (one for image, one for profile sector, one for buttons)

--- scripts/optionscript.js
+++ scripts/optionscript.js
@@ -4,13 +4,13 @@ var studentClub;
 var studentNumber;
 
 function getData() {
-studentGrade = document.getElementById("grade").value;
-studentGender = document.getElementById("gender").value;
-studentClub = document.getElementById("club").value;
-studentNumber = document.getElementById("student#").value;
-// alert(studentGrade + studentGender + studentClub + studentNumber);
-localStorage.setItem("Student Grade:", studentGrade);
-localStorage.setItem("Student Gender:", studentGender);
-localStorage.setItem("Student Club:", studentClub);
-localStorage.setItem("Student Number:", studentNumber);
+	studentGrade = document.getElementById("grade").value;
+	studentGender = document.getElementById("gender").value;
+	studentClub = document.getElementById("club").value;
+	studentNumber = document.getElementById("student#").value;
+	localStorage.setItem("Student Grade:", studentGrade);
+	localStorage.setItem("Student Gender:", studentGender);
+	localStorage.setItem("Student Club:", studentClub);
+	localStorage.setItem("Student Number:", studentNumber);
+	alert("Your profile has been saved.");
 }
