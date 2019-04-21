d6c8601894fd4c64165eac5ce78be289c8d6e9f9
Karnbir S <karnbirsaini@hotmail.com>
2019-04-21 12:31:11 -0400

    Update viewerscript.js
    JS:
    - added annStudentNumber array var which is pulled from local storage
    - added ann prefix to local storage keys to distinguish announcements from student
    - if statement for club now has an "or" for studentnumber which matches the student number from the student profile to the one from the announcement (annClub must be set to blank for this to work)
    
    Possible fix: allow for student number to be checked using an else if statement, but this would have to create a duplicate allTitlesDetails concentation

--- scripts/viewerscript.js
+++ scripts/viewerscript.js
@@ -8,6 +8,7 @@ var annGrade = [];
 var annGender = [];
 var annClub = [];
 var annDateTime = [];
+var annStudentNumber = [];
 var allTitlesDetails = "";
 
 function readStorage() {
@@ -15,19 +16,20 @@ function readStorage() {
 	studentGender = localStorage.getItem("Student Gender:");
 	studentClub = localStorage.getItem("Student Club:");
 	studentNumber = localStorage.getItem("Student Number:");
-	annTitle = JSON.parse(localStorage.getItem("Title:"));
-	annDetails = JSON.parse(localStorage.getItem("Details:"));
-	annGrade = JSON.parse(localStorage.getItem("Grade:"));
-	annGender = JSON.parse(localStorage.getItem("Gender:"));
-	annClub = JSON.parse(localStorage.getItem("Club:"));
-    annDateTime = JSON.parse(localStorage.getItem("DateTime:"))
+	annTitle = JSON.parse(localStorage.getItem("AnnTitle:"));
+	annDetails = JSON.parse(localStorage.getItem("AnnDetails:"));
+	annGrade = JSON.parse(localStorage.getItem("AnnGrade:"));
+	annGender = JSON.parse(localStorage.getItem("AnnGender:"));
+	annClub = JSON.parse(localStorage.getItem("AnnClub:"));
+	annStudentNumber = JSON.parse(localStorage.getItem("AnnStudentNumber:"));
+    annDateTime = JSON.parse(localStorage.getItem("AnnDateTime:"));
 }
 	
 function displayAnn() {
 	for (var index = 0; index < annDetails.length; index += 1) {
 		if (annGrade[index] === studentGrade || annGrade[index] === "allgrades") {
 			if (annGender[index] === studentGender || annGender[index] === "allgenders") {
-				if (annClub[index] === studentClub || annClub[index] === "allstudents") {
+				if (annClub[index] === studentClub || annClub[index] === "allstudents" || annStudentNumber[index] === studentNumber) {
 					allTitlesDetails += "<h2>" + annDateTime[index] + "</h2>" + "<h3>" + annTitle[index] + "</h3>" + "<p>" + annDetails[index] + "</p>";
 				}
 			}
