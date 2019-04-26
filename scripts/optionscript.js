9ddea221d62a77632e20f7e31e9fbc5de95e5e58
Karnbir S <karnbirsaini@hotmail.com>
2019-04-25 22:29:07 -0400

    Added comments to JS, made some minor changes
    A bit excessive, but shows proof of understanding
    
    optionscript.js:
    - set currentProfile var to a empty string
    
    viewerscript.js:
    - renamed allTitlesDetails to allAnnouncements for clarification regarding Time/Date Stamp

--- scripts/optionscript.js
+++ scripts/optionscript.js
@@ -1,12 +1,30 @@
+/* 
+Karnbir Saini
+ICS3UR-B
+April 25, 2019
+Student Options JS - Allows student info to be pulled from HTML input, set into LocalStorage keys + displays current student info at the bottom of the page
+https://github.com/sskarn/announcementsapp-student
+*/
+
 var studentGrade;
 var studentGender;
 var studentClub;
 var studentNumber;
+// creates student profile vars
 var currentGrade;
 var currentGender;
 var currentClub;
 var currentStudentNumber;
-var currentProfile;
+// creates current profile vars to be displayed
+var currentProfile = '';
+// creates currentProfile empty string var
+
+/*
+function getData()
+- loads HTML input from student into JS vars (no array required due to only a single value)
+- sets JS vars into LocalStorage keys
+- displays alert to student to confirm that their settings have been saved
+*/
 
 function getData() {
 	studentGrade = document.getElementById("grade").value;
@@ -20,6 +38,13 @@ function getData() {
 	alert("Your profile has been saved.");
 }
 
+/*
+function displayProfile() (onLoad when webpage is opened to show student their current profile settings, if there are no settings, then it shows as null)
+- current student profile vars are loaded from LocalStorage keys
+- currentProfile string is set to html <p> tags of student grade, gender, club, and student number with <br> for new lines
+- currentprofile div is set to currentProfile string
+*/
+
 function displayProfile() {
 	currentGrade = localStorage.getItem("Student Grade:");
 	currentGender = localStorage.getItem("Student Gender:");
@@ -28,6 +53,11 @@ function displayProfile() {
 	currentProfile = "<p>Grade: " + currentGrade + "<br>Gender: " + currentGender + "<br>Club: " + currentClub + "<br>Student Number: " + currentStudentNumber;
 	document.getElementById("currentprofile").innerHTML = currentProfile;
 }
+
+/*
+function update() (onClick when submit button is clicked in HTML, resulting in settings being updated and currentProfile being updated)
+- runs getData() and displayProfile() in order
+*/
 
 function update() {
 	getData();
--- scripts/viewerscript.js
+++ scripts/viewerscript.js
@@ -1,7 +1,21 @@
+/* 
+Karnbir Saini
+ICS3UR-B
+April 25, 2019
+Student Viewer JS:
+- retrieves all announcement and student variables from LocalStorage
+- uses for loop with if statements to match announcement variables against student variables
+- if match is true:
+	- concentates string (allAnnouncements) variable with the announcement's Time/Date Stamp, Title, and Details
+- displayAnn() function displays string in HTML div element
+https://github.com/sskarn/announcementsapp-student
+*/
+
 var studentGrade;
 var studentGender;
 var studentClub;
 var studentNumber;
+// creates student profile vars
 var annTitle = [];
 var annDetails = [];
 var annGrade = [];
@@ -9,7 +23,14 @@ var annGender = [];
 var annClub = [];
 var annDateTime = [];
 var annStudentNumber = [];
-var allTitlesDetails = "";
+// creates announcement vars (with array)
+var allAnnouncements = "";
+// creates allAnnouncements string var that is concentated with all matching announcements later on
+
+/*
+function readStorage()
+- loads Announcement and Student LocalStorage keys into JS vars (JSON.parse for announcements to support arrays)
+*/
 
 function readStorage() {
 	studentGrade = localStorage.getItem("Student Grade:");
@@ -25,18 +46,37 @@ function readStorage() {
 	annDateTime = JSON.parse(localStorage.getItem("AnnDateTime:"));
 }
 
+/*
+function displayAnn()
+- for loop with parameters including:
+	- an index var being created
+	- index var being less than the length of the annDetails array
+	- index var being concentated by 1 after each loop is completed
+- 3 nested if statements which test for:
+	1. Announcement Grade = Student Grade or if Announcement is intended for all grades
+	2. Announcement Gender = Student Gender or if Announcement is intended for all genders
+	3. Announcement Club = Student Club or if Announcement is intended for all students + or Announcement Student Number = Student Number (club cannot be selected for Student Number targeting to work)
+- If all if statements are true, then the allAnnouncements string is concentated with the matching announcement's Time/Date Stamp, Title, and Details with <h2>, <h3, and <p> tags
+- After the for loop runs, the filteredannouncements div tag is set to the string variable allAnnouncements, resulting in all matching announcements being displayed to the student
+*/
+
 function displayAnn() {
 	for (var index = 0; index < annDetails.length; index += 1) {
 		if (annGrade[index] === studentGrade || annGrade[index] === "allgrades") {
 			if (annGender[index] === studentGender || annGender[index] === "allgenders") {
 				if (annClub[index] === studentClub || annClub[index] === "allstudents" || annStudentNumber[index] === studentNumber) {
-					allTitlesDetails += "<h2>" + annDateTime[index] + "</h2>" + "<h3>" + annTitle[index] + "</h3>" + "<p>" + annDetails[index] + "</p>";
+					allAnnouncements += "<h2>" + annDateTime[index] + "</h2>" + "<h3>" + annTitle[index] + "</h3>" + "<p>" + annDetails[index] + "</p>";
 				}
 			}
 		}
 	}
-	document.getElementById("filteredannouncements").innerHTML = allTitlesDetails;
+	document.getElementById("filteredannouncements").innerHTML = allAnnouncements;
 }
+
+/*
+function start() (onLoad when webpage is opened in order to display filtered announcements in div)
+- loads readStorage() and displayAnn() functions in order
+*/
 
 function start() {
 	readStorage();
